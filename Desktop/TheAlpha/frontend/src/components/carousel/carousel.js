82e869669e15a6e22fba88f90ac9f8f437daf490
felista47 <manotifelista9@gmail.com>
2022-07-16 19:57:07 +0300

    structure of the carousel

---
+++ Desktop/TheAlpha/frontend/src/components/carousel/carousel.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Carousel=()=> {
+    const slides = [
+        "https://picsum.photos/id/1032/900/400",
+        "https://picsum.photos/id/1033/900/400",
+        "https://picsum.photos/id/1037/900/400",
+        "https://picsum.photos/id/1035/900/400",
+        "https://picsum.photos/id/1036/900/400",
+      ]
+
+  return (
+    <div className='carousel'>
+        <div className="carousel-inner">        
+          <div className="carousel-item">
+            <img />
+          </div>        
+        </div>
+      </div>
+
+  )
+}
+
+export default Carousel
