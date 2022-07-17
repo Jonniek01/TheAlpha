9d9cf27a180bfc66683e6fd70f2823cc75687b8b
jonniek01 <jontejonhn@gmail.com>
2022-07-17 06:58:39 +0300

    main todate

--- backend/API/customers/index.js
+++ backend/API/customers/index.js
@@ -1,20 +1,19 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(cors());
 
 
+const PORT = 8082;
 
-const PORT = 8082
-
-app.get('/',(req,res)=>{
-    res.send("Customers microservices")
-})
+app.get('/', (req, res)=>{
+  res.send('Customers microservices');
+});
 
 app.listen(PORT, () => {
-    console.log(`Customers microservices running at port ${PORT}`)
-})
+  console.log(`Customers microservices running at port ${PORT}`);
+});
