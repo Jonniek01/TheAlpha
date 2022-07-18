ce95df72dceee1367ee26ac4c99da2e6d2073559
jonniek01 <jontejonhn@gmail.com>
2022-07-18 09:16:44 +0300

    db

--- backend/API/shopping/database/tables/shopping.sql
+++ backend/API/shopping/database/tables/shopping.sql
@@ -1,21 +1,20 @@
-CREATE DATABASE Shopping
- GO
- use Shopping
 
- create TABLE cartItem(
+USE Shopping
+
+ create TABLE cartitems(
      id int NOT NULL PRIMARY KEY,
      customer_id INT NOT NULL,
      product_id INT NOT NULL,
      count int NOT NULL,
      total VARCHAR(50) NOT NULL,
-     active VARCHAR(50) NOT NULL,
+     date_added DATETIME DEFAULT GETDATE(),
+     active BIT DEFAULT 0
  )
 
 CREATE TABLE orders(
 id int NOT NULL PRIMARY KEY,
 customer_id int NOT NULL,
 content VARCHAR(255) NOT NULL,
-order_date DATE NOT NULL,
-cleared VARCHAR NOT NULL
+order_date DATETIME DEFAULT GETDATE(),
+cleared BIT DEFAULT 0
  )
-
