c8c499c0e7b2b9796b4b8809260e7449f2e4e94e
jonniek01 <jontejonhn@gmail.com>
2022-07-20 23:24:06 +0300

    set up

--- adminpanel/src/App.js
+++ adminpanel/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from './components/sidebar/Sidebar'
+import Header from './components/header/Header'
+import Stats from './components/stats/Stats'
+import RightBar from './components/rightbar/RightBar'
+import Content from './components/content/Content'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <Sidebar/>
+        <Header/>
+        <Stats />
+        <RightBar />
+        <Content />
+
+      </div>
     </div>
   );
 }
---
+++ adminpanel/src/components/rightbar/RightBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RightBar() {
+  return (
+    <div className='right_bar'>RightBar</div>
+  )
+}
+
+export default RightBar
