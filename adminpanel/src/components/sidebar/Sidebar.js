1712b4658449dd8394e1e297fd0b713af67481f7
jonniek01 <jontejonhn@gmail.com>
2022-07-21 07:02:31 +0300

    sidebar

--- adminpanel/src/components/sidebar/Sidebar.js
+++ adminpanel/src/components/sidebar/Sidebar.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import './Sidebar.css'
+import {SiSimpleanalytics } from 'react-icons/si';
+import { BsFillHandbagFill} from 'react-icons/bs';
+import { BsPeopleFill } from 'react-icons/bs';
+import { BiBookAlt } from 'react-icons/bi';
+import { TbLogout } from 'react-icons/tb';
+
+
+
+import {Link} from 'react-router-dom'
 
 function Sidebar() {
   return (
-    <div className='side_bar'>Sidebar</div>
+    <div className='side_bar'>
+      <div className="top">
+        <p className="logo">TheAlpha</p>
+
+      </div>
+      <div className="middle">
+        <Link className='link' to=''><SiSimpleanalytics className='icon'/><span>Overview</span></Link>
+        <Link className='link'  to=''><BsFillHandbagFill className='icon'/><span>Products</span></Link>
+        <Link className='link'  to=''><BsPeopleFill className='icon'/><span>Customers</span></Link>
+        <Link className='link'  to=''><BiBookAlt className='icon'/><span>Orders</span></Link>
+
+        
+      </div>
+      <div className="bottom">
+        <p className='link'><TbLogout/><span></span>Log out</p>
+
+      </div>
+    </div>
   )
 }
 
