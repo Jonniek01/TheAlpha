df4b4de55e821b125654fa4cca51f1b13952bc22
jonniek01 <jontejonhn@gmail.com>
2022-07-22 10:17:12 +0300

    auth complete

--- backend/API/shopping/routes/items.js
+++ backend/API/shopping/routes/items.js
@@ -1,5 +1,8 @@
 /* eslint-disable new-cap */
 const express = require('express');
+const customerAuth = require('../middlewares/customerAuth');
+const adminAuth = require('../middlewares/adminAuth');
+
 const {getItems,
   getItem,
   getItemsUid,
@@ -11,25 +14,25 @@ const items = express.Router();
 
 
 /* GETTING ALL Items*/
-items.get('/',
+items.get('/', adminAuth,
     getItems);
 
 /* CREATE item*/
-items.post('/',
+items.post('/', customerAuth,
     createItem);
 /* GET item BY ID*/
-items.get('/:id',
+items.get('/:id', customerAuth,
     getItem);
 
 /* GET items by user id*/
-items.get('/user/:uid',
+items.get('/user/:uid', customerAuth,
     getItemsUid);
 
 /* UPDATE item*/
-items.patch('/:id',
+items.patch('/:id', customerAuth,
     updateItem);
 /* Delete item by id*/
-items.delete('/:id',
+items.delete('/:id', customerAuth,
     clearItem);
 
 
